daf0e66ae1c933dece16e1e9f406d52c3960bcab
Mohd Sarfraz Saifi <sarfrazcodes@gmail.com>
2026-04-25 18:24:29 +0530

    Time: 0 ms (100%), Space: 14.1 MB (67.8%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,20 +1,15 @@
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end()); // hash table for nums1
-        unordered_set<int> result; // to avoid duplicates
-
-        for(int x : nums2){
-            if(set1.find(x) != set1.end()){ // check if x exists in nums1
-                result.insert(x);
+        vector<int> result;
+        unordered_set<int> ans;
+        for(int x: nums1){
+            for(int y: nums2){
+                if(x==y) 
+                result.push_back(x);
             }
         }
-
-        return vector<int>(result.begin(), result.end()); // convert set to vector
+        ans = unordered_set<int>(result.begin(),result.end());
+        return vector<int>(ans.begin(),ans.end());
     }
 };
