b369b0fd3405a9ad18b0df99f332628c996c6509
Mohd Sarfraz Saifi <sarfrazcodes@gmail.com>
2026-05-01 14:03:39 +0530

    Time: 877 ms (5.22%), Space: 31.3 MB (17.19%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -6,7 +6,15 @@ public:
             int square = x*x;
             results.push_back(square);
         }
-        sort(results.begin(),results.end());
+        for(int i=0;i<results.size()-1;i++){
+            int min_index = i;
+            for(int j=i+1;j<results.size();j++){
+                if(results[j]<results[min_index]){
+                min_index = j;
+            }
+            }
+            swap(results[i],results[min_index]);
+        }
         return results;
     }
 };
