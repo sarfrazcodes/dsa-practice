6d34f1291db5412d9e725ae34c416f9ed0da853b
Mohd Sarfraz Saifi <sarfrazcodes@gmail.com>
2026-06-14 17:22:21 +0530

    Time: 0 ms (100%), Space: 8.7 MB (23.13%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // if(x<0) return 0;
+        if(x<0) return 0;
         long int rev=0;
         int temp =x;
         while(temp!=0){
@@ -9,7 +9,7 @@ public:
             rev = rev * 10 + rem;
             temp/=10;
         }
-        if(x<0) return 0;
+        // if(x<0) return 0;
         return rev==x;
     }
 };
