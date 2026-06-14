4644dab6b1da5cdfbdbca5ce10852ba0e2be5967
Mohd Sarfraz Saifi <sarfrazcodes@gmail.com>
2026-06-14 17:20:27 +0530

    Time: 1 ms (57.43%), Space: 8.7 MB (37.72%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int rev =0;
-        int temp = x;
+        if(x<0) return 0;
+        long long int rev=0;
+        int temp =x;
         while(temp!=0){
-            int rem = temp % 10;
-            if(rev > INT_MAX/10 || rev<INT_MIN/10){
-                return 0;
-            }
-            rev = rev*10 +rem;
-            temp /= 10;
+            int rem = temp %10;
+            rev = rev * 10 + rem;
+            temp/=10;
         }
-        int ans = (rev==x) ?  1 : 0;
-        if(x<0) ans=0;
-        return ans;
+        return rev==x;
     }
 };
